c81d80b85e71fd0ebb211168acc39692a48941d5
sachacq1 <sachacq1@gmail.com>
2025-02-12 10:40:08 -0300

    change dsh

--- frontend/src/views/Admin/dashboard.jsx
+++ frontend/src/views/Admin/dashboard.jsx
@@ -10,10 +10,12 @@ const AdminComerciosList = () => {
         codigo: 0,
         descripcion: "",
         sucursales: [],
-        scoreSTD: { min: 0, max: 0, monto: 0 },
-        scoreDNI: { min: 0, max: 0, monto: 0 }
+        scoreSTD: [],
+        scoreDNI: [],
     });
     const [isEditing, setIsEditing] = useState(false);
+    const [hasScoreSTD, setHasScoreSTD] = useState(false);
+    const [hasScoreDNI, setHasScoreDNI] = useState(false);
 
     const navigate = useNavigate();
 
@@ -58,6 +60,8 @@ const AdminComerciosList = () => {
     const handleEdit = (comercio) => {
         setFormData(comercio);
         setIsEditing(true);
+        setHasScoreSTD(comercio.scoreSTD.length > 0);
+        setHasScoreDNI(comercio.scoreDNI.length > 0);
     };
 
     const resetForm = () => {
@@ -67,10 +71,23 @@ const AdminComerciosList = () => {
             codigo: 0,
             descripcion: "",
             sucursales: [],
-            scoreSTD: { min: 0, max: 0, monto: 0 },
-            scoreDNI: { min: 0, max: 0, monto: 0 }
+            scoreSTD: [],
+            scoreDNI: [],
         });
         setIsEditing(false);
+        setHasScoreSTD(false);
+        setHasScoreDNI(false);
+    };
+
+    const handleScoreChange = (e, scoreType, index, field) => {
+        const updatedScores = [...formData[scoreType]];
+        updatedScores[index][field] = parseInt(e.target.value);
+        setFormData({ ...formData, [scoreType]: updatedScores });
+    };
+
+    const addScore = (scoreType) => {
+        const newScore = { min: 0, max: 0, monto: 0 };
+        setFormData({ ...formData, [scoreType]: [...formData[scoreType], newScore] });
     };
 
     return (
@@ -101,34 +118,48 @@ const AdminComerciosList = () => {
                         <input className="input" name="nombre" type="text" placeholder="Nombre" value={formData.nombre} onChange={(e) => setFormData({ ...formData, nombre: e.target.value })} required />
 
                         <label className="label">Código</label>
-                        <input className="input" name="codigo" type="number" placeholder="Código" value={formData.codigo} onChange={(e) => setFormData({ ...formData, codigo: parseInt(e.target.value) })} required style={{ appearance: "textfield", MozAppearance: "textfield" }} />
+                        <input className="input" name="codigo" type="number" placeholder="Código" value={formData.codigo} onChange={(e) => setFormData({ ...formData, codigo: parseInt(e.target.value) })} required />
 
                         <label className="label">Descripción</label>
                         <textarea className="textarea" name="descripcion" placeholder="Descripción" value={formData.descripcion} onChange={(e) => setFormData({ ...formData, descripcion: e.target.value })} required />
 
-                        {/* Plan STD */}
-                        <h3 className="subtitle is-5">Plan STD</h3>
-                        <div className="field is-grouped">
-                            {!(formData.scoreSTD.min === 0 && formData.scoreSTD.max === 0) && (
-                                <>
-                                    <input className="input" name="std-min" type="number" placeholder="Min" value={formData.scoreSTD.min} onChange={(e) => setFormData({ ...formData, scoreSTD: { ...formData.scoreSTD, min: parseInt(e.target.value) } })} required />
-                                    <input className="input" name="std-max" type="number" placeholder="Max" value={formData.scoreSTD.max} onChange={(e) => setFormData({ ...formData, scoreSTD: { ...formData.scoreSTD, max: parseInt(e.target.value) } })} required />
-                                </>
-                            )}
-                            <input className="input" name="std-monto" type="number" placeholder="Monto" value={formData.scoreSTD.monto} onChange={(e) => setFormData({ ...formData, scoreSTD: { ...formData.scoreSTD, monto: parseInt(e.target.value) } })} required />
-                        </div>
+                        {/* Preguntar si tiene score STD */}
+                        <label className="checkbox">
+                            <input type="checkbox" checked={hasScoreSTD} onChange={(e) => setHasScoreSTD(e.target.checked)} />
+                            Tiene score STD
+                        </label>
 
-                        {/* Plan DNI */}
-                        <h3 className="subtitle is-5">Plan DNI</h3>
-                        <div className="field is-grouped">
-                            {!(formData.scoreDNI.min === 0 && formData.scoreDNI.max === 0) && (
-                                <>
-                                    <input className="input" name="dni-min" type="number" placeholder="Min" value={formData.scoreDNI.min} onChange={(e) => setFormData({ ...formData, scoreDNI: { ...formData.scoreDNI, min: parseInt(e.target.value) } })} required />
-                                    <input className="input" name="dni-max" type="number" placeholder="Max" value={formData.scoreDNI.max} onChange={(e) => setFormData({ ...formData, scoreDNI: { ...formData.scoreDNI, max: parseInt(e.target.value) } })} required />
-                                </>
-                            )}
-                            <input className="input" name="dni-monto" type="number" placeholder="Monto" value={formData.scoreDNI.monto} onChange={(e) => setFormData({ ...formData, scoreDNI: { ...formData.scoreDNI, monto: parseInt(e.target.value) } })} required />
-                        </div>
+                        {hasScoreSTD && (
+                            <>
+                                {formData.scoreSTD.map((score, index) => (
+                                    <div key={index} className="field is-grouped">
+                                        <input className="input" type="number" placeholder="Min" value={score.min} onChange={(e) => handleScoreChange(e, "scoreSTD", index, "min")} required />
+                                        <input className="input" type="number" placeholder="Max" value={score.max} onChange={(e) => handleScoreChange(e, "scoreSTD", index, "max")} required />
+                                        <input className="input" type="number" placeholder="Monto" value={score.monto} onChange={(e) => handleScoreChange(e, "scoreSTD", index, "monto")} required />
+                                    </div>
+                                ))}
+                                <button type="button" className="button is-link" onClick={() => addScore("scoreSTD")}>Agregar Rango STD</button>
+                            </>
+                        )}
+
+                        {/* Preguntar si tiene score DNI */}
+                        <label className="checkbox">
+                            <input type="checkbox" checked={hasScoreDNI} onChange={(e) => setHasScoreDNI(e.target.checked)} />
+                            Tiene score DNI
+                        </label>
+
+                        {hasScoreDNI && (
+                            <>
+                                {formData.scoreDNI.map((score, index) => (
+                                    <div key={index} className="field is-grouped">
+                                        <input className="input" type="number" placeholder="Min" value={score.min} onChange={(e) => handleScoreChange(e, "scoreDNI", index, "min")} required />
+                                        <input className="input" type="number" placeholder="Max" value={score.max} onChange={(e) => handleScoreChange(e, "scoreDNI", index, "max")} required />
+                                        <input className="input" type="number" placeholder="Monto" value={score.monto} onChange={(e) => handleScoreChange(e, "scoreDNI", index, "monto")} required />
+                                    </div>
+                                ))}
+                                <button type="button" className="button is-link" onClick={() => addScore("scoreDNI")}>Agregar Rango DNI</button>
+                            </>
+                        )}
 
                         <button className="button is-primary" type="submit">{isEditing ? "Actualizar Comercio" : "Guardar Comercio"}</button>
                         <button className="button is-light" type="button" onClick={resetForm}>Cancelar</button>
